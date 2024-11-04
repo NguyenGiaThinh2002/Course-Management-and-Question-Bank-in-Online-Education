40919a62154b2b23a7cd53e5da1005a1a6699603
ThinhNguyen2002 <98560432+NguyenGiaThinh2002@users.noreply.github.com>
2024-11-04 09:13:50 +0700

    Update gemini_VN.py

--- AI_For_Classroom/gemini/gemini_VN.py
+++ AI_For_Classroom/gemini/gemini_VN.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import fitz  # PyMuPDF
 
 # Set your API key as an environment variable
-os.environ["GEMINI_API_KEY"] = 'AIzaSyBaDHbFRViW4BQ4MQzpDUmXVzfwUoFywoE'
+os.environ["GEMINI_API_KEY"] = ''
 
 # Configure the Google Generative AI SDK with your API key
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
